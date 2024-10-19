1dbbf8dc147b2b0d6e66b377aa1849c2c3042647
akerelea <akerelea@uni.coventry.ac.uk>
2024-10-19 18:09:46 +0100

    added the remaining methods except rotating face on cube, will be added soon

--- rubik.py
+++ rubik.py
@@ -64,6 +64,12 @@ class Cube:
                         "bottom":"green",
                         "left":"yellow",
                         "right":"white"}
+        self.FRONT = self.GetFaceFromPos("front")
+        self.BACK = self.GetFaceFromPos("back")
+        self.TOP = self.GetFaceFromPos("top")
+        self.BOTTOM = self.GetFaceFromPos("bottom")
+        self.LEFT = self.GetFaceFromPos("left")
+        self.RIGHT = self.GetFaceFromPos("right")
         
     def SetFaces(self, faces):
         self.faces = faces
@@ -167,150 +173,206 @@ class Cube:
                 output_str += "\n--- + --- + --- + --- + --- + --- + --- + --- + ---"
         print(output_str)
 
-    def RotateLU(self, color):
-        FRONT = self.GetFaceFromPos("front")
-        BACK = self.GetFaceFromPos("back")
-        TOP = self.GetFaceFromPos("top")
-        BOTTOM = self.GetFaceFromPos("bottom")
-        LEFT = self.GetFaceFromPos("left")
-        RIGHT = self.GetFaceFromPos("right")
-        for key in self.color_pos:
-            if self.color_pos[key] == color:
-                if key =="front" or key =="back" or key =="top" or key=="bottom":
-                    front_line = self.faces[FRONT].GetLineNodes("L")
-                    top_line = self.faces[TOP].GetLineNodes("L")
-                    bottom_line = self.faces[BOTTOM].GetLineNodes("L")
-                    back_line = self.faces[BACK].GetLineNodes("L")
+    def RotateLU(self, key):
+        #key is the face to be turned
+        if key =="front" or key =="back" or key =="top" or key=="bottom":
+            front_line = self.faces[self.FRONT].GetLineNodes("L")
+            top_line = self.faces[self.TOP].GetLineNodes("L")
+            bottom_line = self.faces[self.BOTTOM].GetLineNodes("L")
+            back_line = self.faces[self.BACK].GetLineNodes("L")
 
-                    self.faces[FRONT].ChangeLine(bottom_line, "L")
-                    self.faces[TOP].ChangeLine(front_line, "L")
-                    self.faces[BACK].ChangeLine(top_line, "L")
-                    self.faces[BOTTOM].ChangeLine(back_line,"L")
+            self.faces[self.FRONT].ChangeLine(bottom_line, "L")
+            self.faces[self.TOP].ChangeLine(front_line, "L")
+            self.faces[self.BACK].ChangeLine(top_line, "L")
+            self.faces[self.BOTTOM].ChangeLine(back_line,"L")
 
-                    self.faces[LEFT].RotateFace("R")
-                
-                elif key=="left":
-                    top_line = self.faces[TOP].GetLineNodes("U")
-                    bottom_line = self.faces[BOTTOM].GetLineNodes("D")
-                    left_line = self.faces[LEFT].GetLineNodes("L")
-                    right_line = self.faces[RIGHT].GetLineNodes("R")
-
-                    self.faces[LEFT].ChangeLine(bottom_line, "L")
-                    self.faces[TOP].ChangeLine(left_line, "U")
-                    self.faces[RIGHT].ChangeLine(top_line, "R")
-                    self.faces[BOTTOM].ChangeLine(right_line,"D")
-
-                    self.faces[BACK].RotateFace("R")
-                
-                elif key=="right":
-                    top_line = self.faces[TOP].GetLineNodes("D")
-                    bottom_line = self.faces[BOTTOM].GetLineNodes("U")
-                    left_line = self.faces[LEFT].GetLineNodes("R")
-                    right_line = self.faces[RIGHT].GetLineNodes("L")
-
-                    self.faces[RIGHT].ChangeLine(bottom_line, "L")
-                    self.faces[TOP].ChangeLine(right_line, "D")
-                    self.faces[LEFT].ChangeLine(top_line, "R")
-                    self.faces[BOTTOM].ChangeLine(left_line,"U")
-
-                    self.faces[FRONT].RotateFace("R")
-
-    def RotateLD(self, color):
-        FRONT = self.GetFaceFromPos("front")
-        BACK = self.GetFaceFromPos("back")
-        TOP = self.GetFaceFromPos("top")
-        BOTTOM = self.GetFaceFromPos("bottom")
-        LEFT = self.GetFaceFromPos("left")
-        RIGHT = self.GetFaceFromPos("right")
-        for key in self.color_pos:
-            if self.color_pos[key] == color:
-                if key =="front" or key =="back" or key =="top" or key=="bottom":
-                    front_line = self.faces[FRONT].GetLineNodes("L")
-                    top_line = self.faces[TOP].GetLineNodes("L")
-                    bottom_line = self.faces[BOTTOM].GetLineNodes("L")
-                    back_line = self.faces[BACK].GetLineNodes("L")
-
-                    self.faces[FRONT].ChangeLine(top_line, "L")
-                    self.faces[TOP].ChangeLine(back_line, "L")
-                    self.faces[BACK].ChangeLine(bottom_line, "L")
-                    self.faces[BOTTOM].ChangeLine(front_line,"L")
-
-                    self.faces[LEFT].RotateFace("L")
-                
-                elif key=="left":
-                    top_line = self.faces[TOP].GetLineNodes("U")
-                    bottom_line = self.faces[BOTTOM].GetLineNodes("D")
-                    left_line = self.faces[LEFT].GetLineNodes("L")
-                    right_line = self.faces[RIGHT].GetLineNodes("R")
-
-                    self.faces[LEFT].ChangeLine(top_line, "L")
-                    self.faces[TOP].ChangeLine(right_line, "U")
-                    self.faces[RIGHT].ChangeLine(bottom_line, "R")
-                    self.faces[BOTTOM].ChangeLine(left_line,"D")
-
-                    self.faces[BACK].RotateFace("L")
-                
-                elif key=="right":
-                    top_line = self.faces[TOP].GetLineNodes("D")
-                    bottom_line = self.faces[BOTTOM].GetLineNodes("U")
-                    left_line = self.faces[LEFT].GetLineNodes("R")
-                    right_line = self.faces[RIGHT].GetLineNodes("L")
-
-                    self.faces[RIGHT].ChangeLine(top_line, "L")
-                    self.faces[TOP].ChangeLine(left_line, "D")
-                    self.faces[LEFT].ChangeLine(bottom_line, "R")
-                    self.faces[BOTTOM].ChangeLine(right_line,"U")
-
-                    self.faces[FRONT].RotateFace("R")
+            self.faces[self.LEFT].RotateFace("R")
         
-    def RotateRU(self, color):
-        FRONT = self.GetFaceFromPos("front")
-        BACK = self.GetFaceFromPos("back")
-        TOP = self.GetFaceFromPos("top")
-        BOTTOM = self.GetFaceFromPos("bottom")
-        LEFT = self.GetFaceFromPos("left")
-        RIGHT = self.GetFaceFromPos("right")
-        for key in self.color_pos:
-            if self.color_pos[key] == color:
-                if key =="front" or key =="back" or key =="top" or key=="bottom":
-                    front_line = self.faces[FRONT].GetLineNodes("L")
-                    top_line = self.faces[TOP].GetLineNodes("L")
-                    bottom_line = self.faces[BOTTOM].GetLineNodes("L")
-                    back_line = self.faces[BACK].GetLineNodes("L")
+        elif key=="left":
+            top_line = self.faces[self.TOP].GetLineNodes("U")
+            bottom_line = self.faces[self.BOTTOM].GetLineNodes("D")
+            left_line = self.faces[self.LEFT].GetLineNodes("L")
+            right_line = self.faces[self.RIGHT].GetLineNodes("R")
 
-                    self.faces[FRONT].ChangeLine(bottom_line, "L")
-                    self.faces[TOP].ChangeLine(front_line, "L")
-                    self.faces[BACK].ChangeLine(top_line, "L")
-                    self.faces[BOTTOM].ChangeLine(back_line,"L")
+            self.faces[self.LEFT].ChangeLine(bottom_line, "L")
+            self.faces[self.TOP].ChangeLine(left_line, "U")
+            self.faces[self.RIGHT].ChangeLine(top_line, "R")
+            self.faces[self.BOTTOM].ChangeLine(right_line,"D")
 
-                    self.faces[LEFT].RotateFace("R")
-                
-                elif key=="left":
-                    top_line = self.faces[TOP].GetLineNodes("U")
-                    bottom_line = self.faces[BOTTOM].GetLineNodes("D")
-                    left_line = self.faces[LEFT].GetLineNodes("L")
-                    right_line = self.faces[RIGHT].GetLineNodes("R")
+            self.faces[self.BACK].RotateFace("R")
+        
+        elif key=="right":
+            top_line = self.faces[self.TOP].GetLineNodes("D")
+            bottom_line = self.faces[self.BOTTOM].GetLineNodes("U")
+            left_line = self.faces[self.LEFT].GetLineNodes("R")
+            right_line = self.faces[self.RIGHT].GetLineNodes("L")
 
-                    self.faces[LEFT].ChangeLine(bottom_line, "L")
-                    self.faces[TOP].ChangeLine(left_line, "U")
-                    self.faces[RIGHT].ChangeLine(top_line, "R")
-                    self.faces[BOTTOM].ChangeLine(right_line,"D")
+            self.faces[self.RIGHT].ChangeLine(bottom_line, "L")
+            self.faces[self.TOP].ChangeLine(right_line, "D")
+            self.faces[self.LEFT].ChangeLine(top_line, "R")
+            self.faces[self.BOTTOM].ChangeLine(left_line,"U")
 
-                    self.faces[BACK].RotateFace("R")
-                
-                elif key=="right":
-                    top_line = self.faces[TOP].GetLineNodes("D")
-                    bottom_line = self.faces[BOTTOM].GetLineNodes("U")
-                    left_line = self.faces[LEFT].GetLineNodes("R")
-                    right_line = self.faces[RIGHT].GetLineNodes("L")
+            self.faces[self.FRONT].RotateFace("R")
 
-                    self.faces[RIGHT].ChangeLine(bottom_line, "L")
-                    self.faces[TOP].ChangeLine(right_line, "D")
-                    self.faces[LEFT].ChangeLine(top_line, "R")
-                    self.faces[BOTTOM].ChangeLine(left_line,"U")
+    def RotateLD(self, key):
+        if key =="front" or key =="back" or key =="top" or key=="bottom":
+            front_line = self.faces[self.FRONT].GetLineNodes("L")
+            top_line = self.faces[self.TOP].GetLineNodes("L")
+            bottom_line = self.faces[self.BOTTOM].GetLineNodes("L")
+            back_line = self.faces[self.BACK].GetLineNodes("L")
 
-                    self.faces[FRONT].RotateFace("R")
+            self.faces[self.FRONT].ChangeLine(top_line, "L")
+            self.faces[self.TOP].ChangeLine(back_line, "L")
+            self.faces[self.BACK].ChangeLine(bottom_line, "L")
+            self.faces[self.BOTTOM].ChangeLine(front_line,"L")
 
+            self.faces[self.LEFT].RotateFace("L")
+        
+        elif key=="left":
+            top_line = self.faces[self.TOP].GetLineNodes("U")
+            bottom_line = self.faces[self.BOTTOM].GetLineNodes("D")
+            left_line = self.faces[self.LEFT].GetLineNodes("L")
+            right_line = self.faces[self.RIGHT].GetLineNodes("R")
+
+            self.faces[self.LEFT].ChangeLine(top_line, "L")
+            self.faces[self.TOP].ChangeLine(right_line, "U")
+            self.faces[self.RIGHT].ChangeLine(bottom_line, "R")
+            self.faces[self.BOTTOM].ChangeLine(left_line,"D")
+
+            self.faces[self.BACK].RotateFace("L")
+        
+        elif key=="right":
+            top_line = self.faces[self.TOP].GetLineNodes("D")
+            bottom_line = self.faces[self.BOTTOM].GetLineNodes("U")
+            left_line = self.faces[self.LEFT].GetLineNodes("R")
+            right_line = self.faces[self.RIGHT].GetLineNodes("L")
+
+            self.faces[self.RIGHT].ChangeLine(top_line, "L")
+            self.faces[self.TOP].ChangeLine(left_line, "D")
+            self.faces[self.LEFT].ChangeLine(bottom_line, "R")
+            self.faces[self.BOTTOM].ChangeLine(right_line,"U")
+
+            self.faces[self.FRONT].RotateFace("R")
+        
+    def RotateRU(self, key):
+        if key =="front" or key =="back" or key =="top" or key=="bottom":
+            front_line = self.faces[self.FRONT].GetLineNodes("R")
+            top_line = self.faces[self.TOP].GetLineNodes("R")
+            bottom_line = self.faces[self.BOTTOM].GetLineNodes("R")
+            back_line = self.faces[self.BACK].GetLineNodes("R")
+
+            self.faces[self.FRONT].ChangeLine(bottom_line, "R")
+            self.faces[self.TOP].ChangeLine(front_line, "R")
+            self.faces[self.BACK].ChangeLine(top_line, "R")
+            self.faces[self.BOTTOM].ChangeLine(back_line,"R")
+
+            self.faces[self.RIGHT].RotateFace("R")
+        
+        elif key=="left":
+            self.RotateLD("right")
+        
+        elif key=="right":
+            self.RotateLD("left")
+
+    def RotateRD(self, key):
+        if key =="front" or key =="back" or key =="top" or key=="bottom":
+            front_line = self.faces[self.FRONT].GetLineNodes("R")
+            top_line = self.faces[self.TOP].GetLineNodes("R")
+            bottom_line = self.faces[self.BOTTOM].GetLineNodes("R")
+            back_line = self.faces[self.BACK].GetLineNodes("R")
+
+            self.faces[self.FRONT].ChangeLine(top_line, "R")
+            self.faces[self.TOP].ChangeLine(back_line, "R")
+            self.faces[self.BACK].ChangeLine(bottom_line, "R")
+            self.faces[self.BOTTOM].ChangeLine(front_line,"R")
+
+            self.faces[self.RIGHT].RotateFace("L")
+        
+        elif key=="left":
+            self.RotateLU("right")
+        
+        elif key=="right":
+            self.RotateLU("left")
+
+    def RotateUL(self, key):
+        #key is the face to be turned
+        if key =="front" or key =="back" or key =="left" or key=="right":
+            front_line = self.faces[self.FRONT].GetLineNodes("U")
+            left_line = self.faces[self.LEFT].GetLineNodes("U")
+            right_line = self.faces[self.RIGHT].GetLineNodes("U")
+            back_line = self.faces[self.BACK].GetLineNodes("D")
+
+            self.faces[self.FRONT].ChangeLine(right_line, "U")
+            self.faces[self.LEFT].ChangeLine(front_line, "U")
+            self.faces[self.BACK].ChangeLine(left_line, "D")
+            self.faces[self.RIGHT].ChangeLine(back_line,"U")
+
+            self.faces[self.TOP].RotateFace("L")
+        
+        elif key=="top":
+            self.RotateLU("left")
+        elif key=="bottom":
+            self.RotateLU("right")
+
+    def RotateUR(self, key):
+        #key is the face to be turned
+        if key =="front" or key =="back" or key =="left" or key=="right":
+            front_line = self.faces[self.FRONT].GetLineNodes("U")
+            left_line = self.faces[self.LEFT].GetLineNodes("U")
+            right_line = self.faces[self.RIGHT].GetLineNodes("U")
+            back_line = self.faces[self.BACK].GetLineNodes("D")
+
+            self.faces[self.FRONT].ChangeLine(left_line, "U")
+            self.faces[self.LEFT].ChangeLine(back_line, "U")
+            self.faces[self.BACK].ChangeLine(right_line, "D")
+            self.faces[self.RIGHT].ChangeLine(front_line,"U")
+
+            self.faces[self.TOP].RotateFace("R")
+        
+        elif key=="top":
+            self.RotateLD("left")
+        elif key=="bottom":
+            self.RotateLD("right")
+    
+    def RotateDL(self, key):
+        #key is the face to be turned
+        if key =="front" or key =="back" or key =="left" or key=="right":
+            front_line = self.faces[self.FRONT].GetLineNodes("D")
+            left_line = self.faces[self.LEFT].GetLineNodes("D")
+            right_line = self.faces[self.RIGHT].GetLineNodes("D")
+            back_line = self.faces[self.BACK].GetLineNodes("U")
+
+            self.faces[self.FRONT].ChangeLine(right_line, "D")
+            self.faces[self.LEFT].ChangeLine(front_line, "D")
+            self.faces[self.BACK].ChangeLine(left_line, "U")
+            self.faces[self.RIGHT].ChangeLine(back_line,"D")
+
+            self.faces[self.BOTTOM].RotateFace("L")
+        
+        elif key=="top" or key=="bottom":
+            self.RotateLD("right")
+        elif key=="bottom":
+            self.RotateLD("left")
+
+    def RotateDR(self, key):
+        #key is the face to be turned
+        if key =="front" or key =="back" or key =="left" or key=="right":
+            front_line = self.faces[self.FRONT].GetLineNodes("D")
+            left_line = self.faces[self.LEFT].GetLineNodes("D")
+            right_line = self.faces[self.RIGHT].GetLineNodes("D")
+            back_line = self.faces[self.BACK].GetLineNodes("U")
+
+            self.faces[self.FRONT].ChangeLine(left_line, "D")
+            self.faces[self.LEFT].ChangeLine(back_line, "D")
+            self.faces[self.BACK].ChangeLine(right_line, "U")
+            self.faces[self.RIGHT].ChangeLine(front_line,"D")
+
+            self.faces[self.BOTTOM].RotateFace("R")
+        
+        elif key=="top":
+            self.RotateLU("right")
+        elif key=="bottom":
+            self.RotateLU("left")
 
 def CreateCube():
     faces = [0,0,0,0,0,0]
@@ -338,17 +400,14 @@ def CreateCube():
                 face.nodes[j][1] = Node([color_pos["front"], color_pos["bottom"]], 1)
                 face.nodes[j][2] = Node([color_pos["front"], color_pos["bottom"], color_pos["right"]], 2)
         faces[i] = face
-        if(rubiks_cube.all_colors[i] == "orange"):
-            front = rubiks_cube.GetFaceFromPos("front")
-            print(rubiks_cube.faces[front].GetColor())
         #print(rubiks_cube.faces[i].GetColor())
         #rubiks_cube.PrintCube()
-    rubiks_cube.SetFaces(faces)
+    rubiks_cube = Cube(faces)
     return rubiks_cube
 
 rubiks_cube = CreateCube()
-rubiks_cube.RotateLU("orange")
-#rubiks_cube.RotateLU("red")
+rubiks_cube.RotateDR("front")
+rubiks_cube.RotateDL("front")
 rubiks_cube.PrintCube()
 #back = rubiks_cube.GetFaceFromPos("back")
 #front = rubiks_cube.GetFaceFromPos("front")
